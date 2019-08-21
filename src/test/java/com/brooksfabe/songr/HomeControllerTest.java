a472ad19ce06f0f8edfbe4df5a3be9d719f35b1d
FabianB14 <32110808+FabianB14@users.noreply.github.com>
2019-08-21 12:09:38 -0700

    completed albums routes

--- src/test/java/com/brooksfabe/songr/HomeControllerTest.java
+++ src/test/java/com/brooksfabe/songr/HomeControllerTest.java
@@ -1,5 +1,6 @@
 package com.brooksfabe.songr;
 
+import com.brooksfabe.songr.contoller.HomeController;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,6 +10,6 @@ public class HomeControllerTest {
     @Test
     public void getReversed() {
 
-    assertEquals("This should return a reversed string","pu s'tahW",HomeController.reversed("What's up"));
+    assertEquals("This should return a reversed string","pu s'tahW", HomeController.reversed("What's up"));
     }
 }
